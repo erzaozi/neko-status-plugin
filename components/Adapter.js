9f43a8fcd8040eb856102668f9177c4a000af41b
ZeroCal Su <3146312184@qq.com>
2024-04-22 17:15:47 +0800

    💫 修复适配器获取

--- components/Adapter.js
+++ components/Adapter.js
@@ -3,7 +3,7 @@ import { _path } from '../model/path.js'
 import { formatNumber } from '../utils/formatTime.js'
 
 export async function getAdapter(bot) {
-    const { app_name: appName, app_version: appVersion } = bot.version || { app_name: 'ICQQ(QQ)', app_version: getIcqqVersion() }
+    const { name: appName, version: appVersion } = bot.version || { name: 'ICQQ(QQ)', version: getIcqqVersion() }
     const sent = formatNumber(await redis.get(`Yz:count:send:msg:bot:${bot.uin}:total`)) || bot.stat?.sent_msg_cnt || 0
     const recv = formatNumber(await redis.get(`Yz:count:receive:msg:bot:${bot.uin}:total`)) || bot.stat?.recv_msg_cnt || 0
     return {
